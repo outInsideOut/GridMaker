7f792ddae2571210459c04690178586837478b32
Fionn O'Connor <fionnoconnor.dev@gmail.com>
2024-10-06 12:38:09 +0100

    refactor to funcs

--- GridMaker_Sketch.py
+++ GridMaker_Sketch.py
@@ -1,55 +1,28 @@
 import json
-from pprint import pprint
+import GridMakerLib
 
 # takes a json <dict>entries and constructs an object from it
 # !! FEED ME GOOD JSON ONLY !!
 
+###############################
+# Qualifying.json:
+# JSON_DATA::sessionResult.leaderBoardLines.racenumber
 
-class Struct:
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
-
-
-def readJsonFile(file):
-    with open(file, 'r') as openFile:
-        return json.load(openFile)
+# EntryList.json:
+# JSON_DATA::entries
+###############################
 
 
 qualiFile = "Qualifying.json"
 entryListFile = "EntryList.json"
 carOrder = []
 
-quali = readJsonFile(qualiFile)
-entryList = readJsonFile(entryListFile)
+quali = GridMakerLib.readJsonFile(qualiFile)
+entryList = GridMakerLib.readJsonFile(entryListFile)
 
-print("Grid order (car numbers):")
-for entry in quali["sessionResult"]["leaderBoardLines"]:
-    entryData = Struct(**entry)
-    carOrder.append(Struct(**(entryData.car)).raceNumber)
-    print(Struct(**(entryData.car)).raceNumber)
+carOrder = GridMakerLib.parseQualiResults(quali)
 
-entries = []
-
-print("EntryList cars with Grid Spots:")
-for entry in entryList["entries"]:
-    entryData = Struct(**entry)
-    try:
-        raceNumber = entryData.raceNumber
-    except:
-        pprint(vars(entryData))
-        entries.append(entryData.__dict__)
-        continue
-
-    if carOrder.__contains__(raceNumber):
-        gridSpot = carOrder.index(raceNumber) + 1
-    else:
-        gridSpot = -1
-
-    print("#: " + str(entryData.raceNumber) +
-          "\t--- Pos: " + str(gridSpot))
-
-    entryData.defaultGridPosition = gridSpot
-    entries.append(entryData.__dict__)
+entries = GridMakerLib.makeGridEntryList(carOrder, entryList)
 
 
 with open("updatedEntryList.json", "w") as json_file:
